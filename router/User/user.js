5753cb7519897743dec2c66073620ba2840da666
JoshuaJohnson8848 <joshuatjohnson255@gmail.com>
2024-03-26 09:02:52 +0530

    Added Auth

--- router/User/user.js
+++ router/User/user.js
@@ -2,17 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../../controller/User/user');
+const isAuth = require('../../middleware/isAuth');
 
 router.get('', userController.getUsers);
 
-router.get('/:id', userController.getUserById);
+router.get('/:id', isAuth, userController.getUserById);
 
-router.post('', userController.addUser);
+router.post('', isAuth, userController.addUser);
 
 router.put('', userController.resetPass);
 
-router.put('/:id', userController.updateUser);
+router.put('/:id', isAuth, userController.updateUser);
 
-router.delete('/:id', userController.deleteUser);
+router.delete('/:id', isAuth, userController.deleteUser);
 
 module.exports = router;
